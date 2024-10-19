7e9a20a62207fd5c863c4f9d2af86c107bdc930e
Tal Frumkin <tal.frumkin@gmail.com>
2024-10-18 22:00:43 -0400

    ui fixes

--- frontend/src/components/ip/IPInput.tsx
+++ frontend/src/components/ip/IPInput.tsx
@@ -4,35 +4,44 @@ interface IPInputProps {
     addCallback: (ip: string, url: string) => void;
 }
 
+const ERROR_MESSAGE_INVALID_IP = 'Invalid IP';
+const ERROR_MESSAGE_INVALID_URL = 'Invalid URL';
+
 const IPInput = ({ addCallback }: IPInputProps) => {
     const [ip, setIP] = useState('');
     const [url, setURL] = useState('');
     const [errorMessageIP, setErrorMessageIP] = useState('');
     const [errorMessageURL, setErrorMessageURL] = useState('');
 
+
     // Regular expressions for validating IPv4 and IPv6
     const ipv4Regex = /^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/;
     const ipv6Regex = /^([0-9a-fA-F]{1,4}:){7}([0-9a-fA-F]{1,4})$/;
     const urlRegex = "^(https?:\\/\\/)?([\\w\\d-]+)\\.([a-z]{2,6})(\\.[a-z]{2,6})?([\\/\\w\\d#?&=.-]*)?$";
 
+    const isIPValid = (value: string) => {
+        return ipv4Regex.test(value) || ipv6Regex.test(value);
+    }
+
+    const isURLValid = (value: string) => {
+        const urlPattern = new RegExp(
+            urlRegex,
+            "i"
+        );
+        return urlPattern.test(value) || ipv4Regex.test(value) || ipv6Regex.test(value);
+    }
+
     // Function to validate IP address
     const validateIP = (value: string) => {
-        if (ipv4Regex.test(value)) {
+        if (isIPValid(value)) {
             setErrorMessageIP(''); // Valid IPv4 address
-        } else if (ipv6Regex.test(value)) {
-            setErrorMessageIP(''); // Valid IPv6 address
         } else {
             setErrorMessageIP('Invalid IP address'); // Invalid IP
         }
     };
 
     const validateURL = (url: string) => {
-        const urlPattern = new RegExp(
-            urlRegex,
-            "i"
-        );
-
-        if (urlPattern.test(url) || ipv4Regex.test(url) || ipv6Regex.test(url)) {
+        if (isURLValid(url)) {
             setErrorMessageURL(''); // Valid URL
         }
         else {
@@ -53,7 +62,6 @@ const IPInput = ({ addCallback }: IPInputProps) => {
         validateURL(value);
     };
 
-
     return (
         <div>
             <div style={{ padding: '20px', display: 'flex', flexDirection: 'column' }}>
@@ -68,6 +76,9 @@ const IPInput = ({ addCallback }: IPInputProps) => {
                     style={{ padding: '10px', marginLeft: '10px' }}
                     placeholder="Enter IPv4 or IPv6"
                 />
+                {errorMessageIP && (
+                    <p style={{ color: 'red', marginTop: '10px' }}>{errorMessageIP}</p>
+                )}
                 <br />
                 <br />
                 <label htmlFor="urlInput" style={{ fontWeight: 'bold' }}>
@@ -81,14 +92,11 @@ const IPInput = ({ addCallback }: IPInputProps) => {
                     style={{ padding: '10px', marginLeft: '10px' }}
                     placeholder="Enter IPv4 or IPv6"
                 />
-                {errorMessageIP && (
-                    <p style={{ color: 'red', marginTop: '10px' }}>{errorMessageIP}</p>
-                )}
                 {errorMessageURL && (
                     <p style={{ color: 'red', marginTop: '10px' }}>{errorMessageURL}</p>
                 )}
             </div>
-            <button disabled={errorMessageIP !== '' || errorMessageURL !== ''} onClick={() => addCallback(ip, url)}>
+            <button disabled={!isIPValid(ip) || !isURLValid(url)} onClick={() => addCallback(ip, url)}>
                 Add
             </button>
         </div>
